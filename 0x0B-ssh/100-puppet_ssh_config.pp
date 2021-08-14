99682700e8410ff5aa53a6d33e9397ebb078a625
Doro-000 <hizkyastekle@gmail.com>
2021-08-14 20:48:14 +0000

    linting

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,15 @@
 # configure ssh config
 
 file_line {'no password':
-  path=> '~/.ssh/config',
-  line=>  'PasswordAuthentication no',
-  match=> '^PasswordAuthentication'
-  append_on_no_match=> 'true',
+  path               => '~/.ssh/config',
+  line               =>  'PasswordAuthentication no',
+  match              => '^PasswordAuthentication'
+  append_on_no_match => true,
 }
 
 file_line {'identity file':
-  path=> '~/.ssh/config',
-  line=>  'IdentityFile ~/.ssh/holberton',
-  match=> '^IdentityFile'
-  append_on_no_match=> 'true',
+  path               => '~/.ssh/config',
+  line               =>  'IdentityFile ~/.ssh/holberton',
+  match              => '^IdentityFile'
+  append_on_no_match => true,
 }
