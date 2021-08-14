82692972c91b1adca0076dd18e5de719712ae1a7
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-15 00:19:10 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,7 @@
 # configure ssh config
 
-exec {'Turn off passwd auth':
-  command => "echo -e 'PasswordAuthentication no\n' >> /etc/ssh/ssh_config",
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-  cwd     => '/home',
-}
-
-exec {'Declare identity file':
-  command=> "echo -e 'IdentityFile ~/.ssh/holbeton\n' >> /etc/ssh/ssh_config",
-  path   => ['/usr/bin', '/usr/sbin', '/bin'],
-  cwd    => '/home',
+file {'create config':
+  path=> '~/.ssh/config',
+  ensure => 'present',
+  content=> "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton",
 }
