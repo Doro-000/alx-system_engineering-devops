19b7d40d484e50f3382e31b4bf3c3cf42df0f4d1
Doro-000 <hizkyastekle@gmail.com>
2021-08-15 14:54:21 +0000

    idk

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,11 @@
 # configure ssh config
 
-file {'/home/.ssh/config':
-  ensure => 'file',
-}
-
 file_line {'Turn off passwd auth':
-  path => '/home/.ssh/config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
-  require => File['/home/.ssh/config'],
 }
 
 file_line {'Declare identity file':
-  path => '/home/.ssh/config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
-  require => File['/home/.ssh/config'],
 }
