c075488d3d2c3abda6e475aef9bc4fd2726153b1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-18 22:58:55 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,19 @@
 # setup nginx
 
+$redirect = "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n"
+$file = '/etc/nginx/sites-available/default'
+
 package {'nginx':
   ensure  => installed,
 }
 
 file {'/var/www/html/index.nginx-debian.html':
   content => 'Holberton School for the win!',
+}
+
+exec {'configure redirect':
+  command => "sed -i '37i\ ${REDIRECT}' ${FILE}",
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
 service {'nginx':
