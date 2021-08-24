ab25a0bb696929ce677d9fd95ba64fb62482bb70
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-24 11:10:07 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,9 +4,14 @@ package {'nginx':
   ensure  => installed,
 }
 
-file_line {'configure custom header':
+file {'/var/www/html/index.nginx-debian.html':
+  content => 'Holberton School for the win!',
+}
+
+file_line {'configure redirection':
   path => '/etc/nginx/sites-available/default',
   after => 'server_name _;',
+  line => "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
   line => '\n\tadd_header X-Served-By $hostname;\n',
 }
 
