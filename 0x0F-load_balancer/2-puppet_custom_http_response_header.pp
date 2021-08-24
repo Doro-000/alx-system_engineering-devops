32e058c9b1128dbf6ff5d554e3e6ff79c11a5848
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-24 11:07:09 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ package {'nginx':
 file_line {'configure custom header':
   path => '/etc/nginx/sites-available/default',
   after => 'server_name _;',
-  line => "\n\tadd_header X-Served-By $hostname;\n",
+  line => '\n\tadd_header X-Served-By $hostname;\n',
 }
 
 service {'nginx':
